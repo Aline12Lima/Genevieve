e96ce1da821f75ce2000d5df2f80bf4db380e9bc
Aline Lima <aline.souza21@outlook.com.br>
2026-02-09 19:50:21 -0300

    refactor: organiza estrutura principal do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import "./index.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./layouts/MainLayout";
 import { Home } from "./pages/Home";
-import { About } from "./pages/About";
-import { Projects } from "./pages/Projects";
-import { Contact } from "./pages/Contact";
 
-export function App() {
+import { Contact } from "./pages/Contact";
+import { ScrollToTop } from "./components/scrollTop/ScrollToTop";
+
+function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop /> {/* 👈 AGORA SIM */}
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/projetos" element={<Projects />} />
+
           <Route path="/contato" element={<Contact />} />
         </Routes>
       </Layout>
